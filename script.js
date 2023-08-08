062d246d8055ae126fdc51e88cb0a1fdf6379ede
Nisser111 <kulakmilosz2006@gmail.com>
2023-08-08 19:22:03 +0200

    Final date info under title

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const hoursDisplay = document.querySelector("#hours-display");
 const minutesDisplay = document.querySelector("#minutes-display");
 const secoundsDisplay = document.querySelector("#secounds-display");
 
-const countdownDate = new Date("Mar 3, 2024 00:00:00").getTime();
+let finalDate = new Date("March 3, 2024 05:15:17");
+
+const countdownDate = finalDate.getTime();
 const countdown = setInterval(() => {
   const now = new Date().getTime();
   let timeLeft = countdownDate - now;
@@ -16,14 +18,53 @@ const countdown = setInterval(() => {
 
   // Set current values
   daysDisplay.value = days;
-  hoursDisplay.value = hours;
-  minutesDisplay.value = minutes;
-  secoundsDisplay.value = seconds;
+  hoursDisplay.value = hours.toString().padStart(2, "0");
+  minutesDisplay.value = minutes.toString().padStart(2, "0");
+  secoundsDisplay.value = seconds.toString().padStart(2, "0");
 
   // When countdown is over
   if (timeLeft < 0) clearInterval(countdown);
 }, 1000);
 
-window.onload = () => {
-  countdown();
+// Transform data from Date to readable string
+function getFinalDateInfo(date) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const mounths = [
+    "January",
+    "Fabruary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let nameOfDay = days[date.getDay()];
+  let day = date.getDate();
+  let nameOfMounth = mounths[date.getMonth()];
+  let year = date.getFullYear();
+  let seconds = date.getSeconds().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let hours = date.getHours().toString().padStart(2, "0");
+
+  dateInfo = `${nameOfDay}, ${day} ${nameOfMounth} ${year} ${hours}:${minutes}:${seconds}`;
+  return dateInfo;
+}
+
+window.onload = function () {
+  // Render readable string
+  const finalDateDisplay = document.querySelector("#countdown-date-display");
+  finalDateDisplay.innerHTML = getFinalDateInfo(finalDate);
 };

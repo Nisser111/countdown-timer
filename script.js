48329f9e1b65ff43c189b0be0491129e5b8e9e3e
Nisser111 <kulakmilosz2006@gmail.com>
2023-08-18 17:09:21 +0200

    Fixed problem with final date load

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const now = new Date();
+let finalDate =
+new Date(localStorage.getItem("finalDate")) ||
+new Date(now.getFullYear(), 11, 31, 0, 0, 0);
+
 function reloadCountdownTimer(date) {
   const daysDisplay = document.querySelector("#days-display");
   const hoursDisplay = document.querySelector("#hours-display");
@@ -112,11 +117,6 @@ window.onload = function () {
   const buttons = document.querySelectorAll("#settings .buttons-wrapper button");
   const changeThemeButton = document.querySelector(".theme-container header");
   const settingsShortcutsButtons = document.querySelectorAll("#settings .shortcuts-buttons button");
-
-  const now = new Date();
-  let finalDate =
-  new Date(localStorage.getItem("finalDate")) ||
-  new Date(now.getFullYear(), 11, 31, 0, 0, 0);
   
   const avaiavbleColorScheme = [
     "light",
